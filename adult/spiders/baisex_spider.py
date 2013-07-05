af7ce76341d7b93cba5c8a6e202df3726d1f90d3
Chaney Lee <chaneylee86@gmail.com>
2013-07-05 22:19:28 +0800

    add the method of creating urls

--- adult/spiders/baisex_spider.py
+++ adult/spiders/baisex_spider.py
@@ -48,23 +48,28 @@ class BaiSexSpider(BaseSpider):
 	##total number of the section
 	#total_pages = 407 
 	
-	#no mosaic 2 
+	##no mosaic 2 
+	##movie section relative page url prifix
+	#relative_url_prifix = "forum-60-"
+	##total number of the section
+	#total_pages = 673 
+	
+	##X-rated  
+	##movie section relative page url prifix
+	#relative_url_prifix = "forum-111-"
+	##total number of the section
+	#total_pages = 245  
+
+	#HD 2  
 	#movie section relative page url prifix
-	relative_url_prifix = "forum-60-"
+	relative_url_prifix = "forum-182-"
 	#total number of the section
-	total_pages = 673 
-
-
-
+	total_pages = 44  
+	
 	name = "baisex"
 	allowed_domains = ["173.245.71.203"]
 	start_urls = []
 	
-	#start_urls = [
-	#		"http://173.245.71.203/forum-109-1.html"
-	#	]
-
-
 	#xpath string
 	#title_xpath = "//form[@id=\'moderate\']/table/tbody[position()>9]//th/a[1]/text()"
 	#link_xpath = "//form[@id=\'moderate\']/table/tbody[position()>9]//th/a[1]/@href"
@@ -75,13 +80,9 @@ class BaiSexSpider(BaseSpider):
 	link_xpath = "//form[@id=\'moderate\']/table/tbody//th/a[1]/@href"
 	date_xpath = "//form[@id=\'moderate\']/table/tbody//td[2]/em/span/text()"
  	
-	#create the urls need to be extracted
 	def __init__(self):
-		page_num = 1 
-		while page_num <= self.total_pages:
-			self.start_urls.append(self.baisex_url+self.relative_url_prifix+str(page_num)+".html")
-			page_num += 1
-	
+		self.__create_urls()
+
 
 	def parse(self,response):
 		hxs = HtmlXPathSelector(response)
@@ -96,7 +97,14 @@ class BaiSexSpider(BaseSpider):
 			yield item
 	
 
-		
+	#create the urls need to be extracted
+	def __create_urls(self):
+		page_num = 1 
+		while page_num <= self.total_pages:
+			self.start_urls.append(self.baisex_url+self.relative_url_prifix+str(page_num)+".html")
+			page_num += 1
+	
+			
 
 
 
