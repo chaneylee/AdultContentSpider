9210fbb70ddf97d329f73f8e38ba13607b62e697
Chaney Lee <chaneylee86@gmail.com>
2013-07-05 11:28:06 +0800

    revise code,ready to crawl the hole section

--- adult/spiders/baisex_spider.py
+++ adult/spiders/baisex_spider.py
@@ -5,28 +5,39 @@ from adult.items import AdultItem
 class BaiSexSpider(BaseSpider):
 	#baisex ip
 	baisex_url = "http://173.245.71.203/"
-	pages_num = 200 
+	
+	#total number of the section
+	total_pages = 531 
+
+	#movie section relative page url prifix
+	relative_url_prifix = "forum-109-"
 
 	name = "baisex"
 	allowed_domains = ["173.245.71.203"]
+	start_urls = []
 	
-	start_urls = [
-			"http://173.245.71.203/forum-109-1.html"
-		]
-	
-	#create the urls need to be extracted 
-	def __init__(self):
-		i = 2 
-		while i < self.pages_num:
-			self.start_urls.append(self.baisex_url+"forum-109-"+str(i)+".html")
-			i = i+1
-	
-	
-	#const xpath string
-	title_xpath = "//form[@id=\'moderate\']/table/tbody[position()>9]//th/a[1]/text()"
-	link_xpath = "//form[@id=\'moderate\']/table/tbody[position()>9]//th/a[1]/@href"
-	date_xpath = "//form[@id=\'moderate\']/table/tbody[position()>9]//td[2]/em/span/text()"
+	#start_urls = [
+	#		"http://173.245.71.203/forum-109-1.html"
+	#	]
+
+
+	#xpath string
+	#title_xpath = "//form[@id=\'moderate\']/table/tbody[position()>9]//th/a[1]/text()"
+	#link_xpath = "//form[@id=\'moderate\']/table/tbody[position()>9]//th/a[1]/@href"
+	#date_xpath = "//form[@id=\'moderate\']/table/tbody[position()>9]//td[2]/em/span/text()"
  	
+	#xpath string
+	title_xpath = "//form[@id=\'moderate\']/table/tbody//th/a[1]/text()"
+	link_xpath = "//form[@id=\'moderate\']/table/tbody//th/a[1]/@href"
+	date_xpath = "//form[@id=\'moderate\']/table/tbody//td[2]/em/span/text()"
+ 	
+	#create the urls need to be extracted
+	def __init__(self):
+		page_num = 1 
+		while page_num <= self.total_pages:
+			self.start_urls.append(self.baisex_url+self.relative_url_prifix+str(page_num)+".html")
+			page_num += 1
+	
 
 	def parse(self,response):
 		hxs = HtmlXPathSelector(response)
